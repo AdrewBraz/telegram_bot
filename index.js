76767232376e074168c40bb42a82299a35fa0ce0
unknown <andbrazhnikov@gmail.com>
2021-11-30 15:29:17 +0300

    Bot change

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ import fs from 'fs'
 import { promisify } from 'util'
 import TelegramBot from 'node-telegram-bot-api'
 import path from 'path'
+import { format } from 'date-fns'
 
-
-const bot = new TelegramBot(BOT_TOKEN, { polling: true })
+const BOT_TOKEN = '2064238273:AAFEVdDRRvewDRkXAW4_Xow-RZ-47eiAMMc'
+const bot = new TelegramBot(BOT_TOKEN, { polling: true });
+const month = format(new Date, 'MM');
+const diskPath = `\Z:\\Pol\\${month}`
 
 const patientData = {
     fio: '',
@@ -17,12 +20,15 @@ const regexp = /([А-ЯЁа-яё]+)\s([А-ЯЁа-яё]+)\s([А-ЯЁа-яё]+)/
 const replacer = (match, p1, p2, p3) => [p1[0].toUpperCase()+p1.slice(1), p2[0].toUpperCase(), p3[0].toUpperCase()].join(' ')
 const writeFilePromise = promisify(fs.writeFile);
 const download = async (url, path) => {
+    if(!fs.existsSync(`${path}\\${patientData.fio}`)){
+        fs.mkdirSync(`${path}\\${patientData.fio}`)
+    }
     return fetch(url)
       .then(x => {
           console.log(x)
           return x.arrayBuffer()
       })
-      .then(x => writeFilePromise(path, Buffer.from(x)));
+      .then(x => writeFilePromise(`${diskPath}\\${patientData.fio}\\${patientData.fio}.jpeg`, Buffer.from(x)));
 
   };
 
@@ -53,9 +59,7 @@ bot.on('photo', async (doc) => {
     );
     const res2 = await res.json();
     const { file_path } = res2.result
-    patientData.photoUrl
-    const downloadURL = 
-      `https://api.telegram.org/file/bot${BOT_TOKEN}/${file_path}`;
-    await download(downloadURL, path.join(__dirname, `${patientData.fio}.png`))
+    patientData.photoUrl = `https://api.telegram.org/file/bot${BOT_TOKEN}/${file_path}`
+    await download(patientData.photoUrl, diskPath)
 
 })

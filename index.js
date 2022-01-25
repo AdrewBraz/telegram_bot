40a281fbcd8359913c2dd5d0e301b899b9348dc8
unknown <andbrazhnikov@gmail.com>
2022-01-25 16:28:28 +0300

    Bot change

--- index.js
+++ index.js
@@ -5,8 +5,7 @@ import { format } from 'date-fns'
 
 const BOT_TOKEN = '2064238273:AAFEVdDRRvewDRkXAW4_Xow-RZ-47eiAMMc'
 const bot = new TelegramBot(BOT_TOKEN, { polling: true });
-const month = format(new Date, 'MM');
-const diskPath = `\Z:\\Pol\\${month}`
+const diskPath = `\Z:\\Pol`
 
 const patientData = {
     fio: '',
@@ -17,23 +16,21 @@ const regexp = /([А-ЯЁа-яё]+)\s([А-ЯЁа-яё]+)\s([А-ЯЁа-яё]+)/
 
 const replacer = (match, p1, p2, p3) => [p1[0].toUpperCase()+p1.slice(1), p2[0].toUpperCase(), p3[0].toUpperCase()].join(' ')
 
-bot.onText(/([А-ЯЁа-яё]+)/, (msg) => {
-    const { chat: { id } } = msg;
-    bot.sendMessage(id, `Введите ФИО пациента с помощью команды /FIO Например: /FIO Иванов Иван Иванович`)
-})
+
 
 bot.onText(/\/start/, (msg) => {
     const { chat: { id } } = msg;
     bot.sendMessage(id, `Введите ФИО пациента с помощью команды /FIO Например: /FIO Иванов Иван Иванович`)
 })
 
-bot.onText(/\/FIO (.+)/, (msg, [source, match]) => {
+bot.onText(/\/FIO (.+)/i, (msg, [source, match]) => {
     const { chat: { id } } = msg;
     const fio = match.replace(regexp, replacer)
     patientData.fio = fio;
     bot.sendMessage(id, `Спасибо 😘😘😘. Теперь нам нужно фото протокола. 
     Прикрепите фото протокола и отправьте его`)
 })
+
 
 bot.on('photo', async (doc) => {
     console.log(doc)
